a1ec5aceb8bc21bff2868221760328c80b357b91
susanka068 <sathinandimajumder068@gmail.com>
2021-10-05 11:13:31 +0530

    added manhattan distance instead of euclidean distance

--- knn.py
+++ knn.py
@@ -9,11 +9,16 @@ def euclidean_distance(sample1, sample2):
         distance += (sample1[i] - sample2[i]) ** 2
     return math.sqrt(distance)
 
+def manhattan_distance(sample1, sample2):
+    distance = 0 
+    for i in range(len(sample1)):
+        distance += abs(sample1[i] - sample2[i])
+    return distance
 
 def predict(X_train, y_train, query, n_neighbors):
     distances = []
     for idx, sample in enumerate(X_train):
-        distance = euclidean_distance(sample, query)
+        distance = manhattan_distance(sample, query)
         distances.append((distance, idx))
     sorted_distances = sorted(distances)
     k_nearest = sorted_distances[:n_neighbors]
